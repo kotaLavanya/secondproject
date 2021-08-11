4c071a0ec364d74c40ba91f89201596eab360364
kotaLavanya <88623974+kotaLavanya@users.noreply.github.com>
2021-08-11 13:58:32 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestClient;
+
+namespace UnitTestcalculate
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //Arrange  --- variables and provide values
+
+            int num = 20;
+            int deno = 4;
+            int expectedresult = 4;
+
+
+            // Act ------  call the function to test
+
+            Program p = new Program();
+           int actualResult= p.divide(num, deno);
+
+            //Assert  ----  getting the result
+
+            Assert.AreEqual(expectedresult, actualResult);
+
+        }
+    }
+}
